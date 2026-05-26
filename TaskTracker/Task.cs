b31d34e4eebb7dc647c4164aedf6926d814c41db
Mykyta D <nikita.derii@gmail.com>
2026-05-26 14:08:24 +0200

    Created Task model

---
+++ TaskTracker/Task.cs
@@ -0,0 +1,22 @@
+﻿namespace TaskTracker
+{
+    internal class Task
+    {
+        public int Id { get; private set; }
+        public string Title;
+        public string Description;
+        public int Priority;
+        public string Category;
+        public DateOnly DueDate;
+
+        public Task(int id, string title, string description, int priority, string category, DateOnly dueDate)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Priority = priority;
+            Category = category;
+            DueDate = dueDate;
+        }
+    }
+}
