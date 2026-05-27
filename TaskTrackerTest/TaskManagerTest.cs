ddcfed44284056d537a525bea013176ebb8f23b3
Mykyta D <nikita.derii@gmail.com>
2026-05-27 17:47:14 +0200

    Test: added a failing test for task deletion

---
+++ TaskTrackerTest/TaskManagerTest.cs
@@ -0,0 +1,32 @@
+﻿using TaskTracker;
+using Task = TaskTracker.Task;
+
+namespace TaskTrackerTest
+{
+    public class TaskManagerTest
+    {
+        [Fact]
+        public void ThreeTasks_DeleteSecond_CorrectTaskDeleted()
+        {
+            TaskManager taskManager = new();
+            TaskBuilder taskBuilder = new();
+
+            List<Task> tasks = new();
+            for (int i = 0; i < 3; i++)
+            {
+                taskBuilder.Reset();
+                var task = taskBuilder.GetResult();
+                taskManager.Add(task);
+                tasks.Add(task);
+            }
+
+            bool deleted = taskManager.DeleteTask(tasks[1].Id);
+            Assert.True(deleted);
+
+            var remainingTasks = taskManager.GetTasks();
+            Assert.Contains(tasks[0], remainingTasks);
+            Assert.DoesNotContain(tasks[1], remainingTasks);
+            Assert.Contains(tasks[2], remainingTasks);
+        }
+    }
+}
