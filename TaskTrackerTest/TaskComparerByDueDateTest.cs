dd17733e5078a60acf7f486edbab6a902ed7751e
Mykyta D <nikita.derii@gmail.com>
2026-05-30 16:11:27 +0200

    Test: add tests for the sorting logic.

---
+++ TaskTrackerTest/TaskComparerByDueDateTest.cs
@@ -0,0 +1,39 @@
+﻿using TaskTracker;
+using TaskTracker.TaskSortStrategies;
+
+namespace TaskTrackerTest
+{
+    public class TaskComparerByDueDateTest
+    {
+        [Fact]
+        public void GivenTasks_ThenSort_ReturnsSorted()
+        {
+            TaskManager taskManager = new TaskManager();
+            TaskBuilder taskBuilder = new();
+
+            taskBuilder.Reset();
+            taskBuilder.SetDueDate(DateOnly.Parse("02.02.2025"));
+            taskManager.AddTask(taskBuilder.GetResult());
+
+            taskBuilder.Reset();
+            taskBuilder.SetDueDate(DateOnly.Parse("01.01.2025"));
+            taskManager.AddTask(taskBuilder.GetResult());
+
+            taskBuilder.Reset();
+            // No due date set; must be at the end of the list
+            taskManager.AddTask(taskBuilder.GetResult());
+
+            taskBuilder.Reset();
+            taskBuilder.SetDueDate(DateOnly.Parse("02.02.2026"));
+            taskManager.AddTask(taskBuilder.GetResult());
+
+            var tasks = taskManager.GetTasks();
+            var sortedTasks = taskManager.SortTasks(new TaskComparerByDueDate());
+
+            Assert.Equal(tasks[1], sortedTasks[0]);
+            Assert.Equal(tasks[0], sortedTasks[1]);
+            Assert.Equal(tasks[3], sortedTasks[2]);
+            Assert.Equal(tasks[2], sortedTasks[3]);
+        }
+    }
+}
---
+++ TaskTrackerTest/TaskComparerByPriorityTest.cs
@@ -0,0 +1,39 @@
+﻿using TaskTracker;
+using TaskTracker.TaskSortStrategies;
+
+namespace TaskTrackerTest
+{
+    public class TaskComparerByPriorityTest
+    {
+        [Fact]
+        public void GivenTasks_ThenSort_ReturnsSorted()
+        {
+            TaskManager taskManager = new TaskManager();
+            TaskBuilder taskBuilder = new();
+
+            taskBuilder.Reset();
+            taskBuilder.SetPriority(2);
+            taskManager.AddTask(taskBuilder.GetResult());
+
+            taskBuilder.Reset();
+            taskBuilder.SetPriority(1);
+            taskManager.AddTask(taskBuilder.GetResult());
+
+            taskBuilder.Reset();
+            // No priority set; must be at the end of the list
+            taskManager.AddTask(taskBuilder.GetResult());
+
+            taskBuilder.Reset();
+            taskBuilder.SetPriority(3);
+            taskManager.AddTask(taskBuilder.GetResult());
+
+            var tasks = taskManager.GetTasks();
+            var sortedTasks = taskManager.SortTasks(new TaskComparerByPriority());
+
+            Assert.Equal(tasks[1], sortedTasks[0]);
+            Assert.Equal(tasks[0], sortedTasks[1]);
+            Assert.Equal(tasks[3], sortedTasks[2]);
+            Assert.Equal(tasks[2], sortedTasks[3]);
+        }
+    }
+}
